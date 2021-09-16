f9ea48d18f503c282ad22fae613ea63d4715bc6e
cchang373 <30272503+cchang373@users.noreply.github.com>
2021-09-16 09:38:07 -0400

    Update EC_fp

--- molecule_rxn/__init__.py
+++ molecule_rxn/__init__.py
@@ -1,3 +1,5 @@
-__version__ = "0.0.1"
-__author__ = 'Chaoyi Chang'
-__credits__ = 'Georgia Institute of Technology'
+#__version__ = "0.0.1"
+#__author__ = 'Chaoyi Chang'
+#__credits__ = 'Georgia Institute of Technology'
+
+from .EC_fp import EC_fp
