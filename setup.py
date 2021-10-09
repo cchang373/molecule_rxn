bd950a4313eaa36f7b09bf9751d09d84708c4a4f
cchang373 <30272503+cchang373@users.noreply.github.com>
2021-10-09 15:26:07 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     install_requires=['networkx',
                       'imolecule',
                       'ase',
+                      'bitarray'
                       ],
 
     classifiers=[
